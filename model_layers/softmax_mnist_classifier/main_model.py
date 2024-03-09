940fc37ce577cd6033de2a779fbeabbbe66fa666
raillylinker <raillylinker@gmail.com>
2024-03-09 09:18:39 +0900

    -

--- model_layers/softmax_mnist_classifier/main_model.py
+++ model_layers/softmax_mnist_classifier/main_model.py
@@ -3,25 +3,22 @@ from torch import nn
 
 class MainModel(nn.Module):
     """
-    description : 선형 회귀 모델
-    input_shape : [[x1, x2, x3], [x1, x2, x3], ...]
-    output_shape : [[y1], [y1], ...]
+    description : Softmax 로 MNIST 분류
+    input_shape : (784,)
+    output_shape : (10,)
     """
 
     def __init__(self):
         super().__init__()
         # 모델 내 레이어
-        self.layer = nn.Sequential(
-            nn.Linear(3, 1),
-            nn.Sigmoid()
-        )
+        self.linear = nn.Linear(784, 10, bias=True)
 
         self._init_weights()
 
     def _init_weights(self):
-        nn.init.xavier_uniform_(self.layer[0].weight)
-        self.layer[0].bias.data.fill_(0.01)
+        nn.init.xavier_uniform_(self.linear.weight)
+        self.linear.bias.data.fill_(0.01)
 
     def forward(self, model_in):
-        model_out = self.layer(model_in)
+        model_out = self.linear(model_in)
         return model_out
